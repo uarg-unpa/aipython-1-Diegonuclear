9234a0bed7fb4909ce15cfe1f1868cc1345d41f8
Diegonuclear <147439842+Diegonuclear@users.noreply.github.com>
2023-10-20 21:58:11 +0000

    actualizo viernes

---
+++ entrega/G1/viernes.py
@@ -0,0 +1,34 @@
+"""print ("viernes en AIPython")
+print ("taller","AIPython","viernes")
+print("uno" " " "dos") 
+print("viernes", end="\n")
+print("taller","AIPython","viernes",sep="_")
+
+print("taller","AIPython","viernes",end="*")
+
+print()
+
+print("taller","AIPython","viernes",end="\t")
+
+print()
+
+print("taller","AIPython","viernes",end="\n")
+
+print("dias\n tipico \n ejercicios")
+
+print("esto es un\n salto de linea")
+
+print("la funcion \"print\"...")
+
+print('la funcion "print"...')
+"""
+
+
+print ("Las máquinas me sorprenden con mucha frecuencia")
+print ("")
+print("hola como va?")
+print("23", 23) # "texto o str", numero entero 
+print("“Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano.”")
+print("DIEGO","GOMEZ", "38")
+print("DIEGO","GOMEZ", "38", sep="_")
+print("hola", "mi edad es:", 40)
